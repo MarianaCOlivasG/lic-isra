1f291e6d5aa2ae251a82b624c5dbda41f92e585a
Mariana Olivas <olivasgarcia031096@gmail.com>
2024-10-31 13:56:27 -0500

    port mysql

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function connectDatabase() {
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
             database: process.env.MYSQL_DATABASE,
-            port: process.env.MYSQL_PORT
+            port: +process.env.MYSQL_PORT
         });
         
         console.log('Database connected');
